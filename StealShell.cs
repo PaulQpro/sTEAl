d2ed2c766c00532f5deaa37b1d275500603a1ec0
PaulQpro <paulqpro@mail.ru>
2023-12-20 20:01:30 +0300

    Added Proto-Shell

--- StealShell.cs
+++ StealShell.cs
@@ -1,9 +1,37 @@
 using System;
+using System.IO;
 using Steal;
 namespace Default{
 	public class Program{
 		public static void Main(){
-			new StealLang("Line \"Hello World!\"\nVar str Type Decimal Value .005\nLine $str").Run();
+			Console.WriteLine(
+				"Welcome into Steal (Strictly Typed Entertaiment Algorithmic Language) v0.1 Shell\n" +
+				"Enter file(realtive or absolute path) to execute"
+			);
+			string src;
+            while (true)
+            {
+				string name = Console.ReadLine();
+                if (File.Exists(name))
+                {
+					src = File.ReadAllText(name);
+					break;
+                }
+				else if (Directory.Exists(name))
+                {
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"{name} is a folder");
+					Console.ResetColor();
+                }
+                else
+                {
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"File {name} does not exists");
+					Console.ResetColor();
+				}
+            }
+			new StealLang(src).Run();
+			Console.WriteLine("Done!\nPress any key to exit ...");
 			Console.ReadKey();
 		}
 	}
